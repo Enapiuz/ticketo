82b32a56a803348e8f991e589d96255a8161ece3
Enapiuz <enapiuz@gmail.com>
2016-08-13 23:52:08 +0300

    get ticket w/o comments

--- bootstrap.sql
+++ bootstrap.sql
@@ -5,3 +5,28 @@ CREATE DATABASE ticketo
   WITH ENCODING='UTF8'
        OWNER=ticketo
        CONNECTION LIMIT=-1;
+
+
+CREATE TABLE public.tickets
+(
+  id          INTEGER NOT NULL DEFAULT nextval('tickets_id_seq' :: REGCLASS),
+  create_date DATE    NOT NULL,
+  change_date DATE    NOT NULL,
+  topic       TEXT    NOT NULL,
+  content     TEXT,
+  email       TEXT,
+  state       INTEGER NOT NULL,
+  CONSTRAINT id_primary PRIMARY KEY (id)
+);
+
+CREATE TABLE public.ticket_comments
+(
+  id          INTEGER NOT NULL DEFAULT nextval('ticket_comments_id_seq' :: REGCLASS),
+  ticket_id   INTEGER NOT NULL,
+  create_date DATE    NOT NULL,
+  email       TEXT    NOT NULL,
+  content     TEXT    NOT NULL,
+  CONSTRAINT ticket_id_foreign FOREIGN KEY (ticket_id)
+  REFERENCES public.tickets (id) MATCH SIMPLE
+  ON UPDATE CASCADE ON DELETE CASCADE
+);
--- src/ticketo.py
+++ src/ticketo.py
@@ -1,5 +1,6 @@
-from flask import Flask, g, jsonify
+from flask import Flask, g, jsonify, abort, request
 import psycopg2
+import pylibmc
 
 app = Flask(__name__)
 
@@ -7,19 +8,29 @@ app = Flask(__name__)
 def get_db():
     if not hasattr(g, 'postgres'):
         conn = psycopg2.connect(database='ticketo', user='ticketo', password='1', host='localhost', port=5432)
-        cur = conn.cursor()
         g.postgres = conn
-        g.postgres_cursor = cur
-    return g.postgres_cursor
+    return g.postgres
+
+
+def get_mc():
+    if not hasattr(g, 'memcache'):
+        mc = pylibmc.Client(["127.0.0.1"], binary=True, behaviors={"tcp_nodelay": True, "ketama": True})
+        g.memcache = mc
+    return g.memcache
 
 
 @app.teardown_appcontext
 def close_connection(exception):
     pg = getattr(g, 'postgres', None)
-    pgcur = getattr(g, 'postgres_cursor', None)
-    if pg is not None and pgcur is not None:
-        pgcur.close()
+    if pg is not None :
         pg.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    response = jsonify({'code': 404,'message': 'Not found'})
+    response.status_code = 404
+    return response
 
 
 @app.route('/')
@@ -44,7 +55,23 @@ def ticket_add_comment(ticket_id):
 
 @app.route('/api/v1/tickets/<int:ticket_id>')
 def ticket_get(ticket_id):
-    return 'not implemented'
+    mc = get_mc()
+    ticket = mc.get(str(ticket_id))
+    if ticket is not None:
+        return jsonify(ticket)
+
+    db = get_db().cursor()
+    db.execute('SELECT * FROM tickets WHERE id = %s;', [ticket_id])
+    ticket = db.fetchone()
+
+    if ticket is None:
+        abort(404)
+    else:
+        col_names = [el[0] for el in db.description]
+        data = dict(zip(col_names, ticket))
+        mc[str(ticket_id)] = data
+        res = jsonify(data)
+        return res
 
 
 if __name__ == '__main__':
